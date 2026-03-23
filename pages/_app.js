1b24ed0cfd2b2c1f1b391b8caf7fa3cb297d4426
Fabian <65469588+fabianjuppe@users.noreply.github.com>
2026-03-23 10:58:40 +0100

    fetch api data

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,32 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+
+async function fetcher(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await response.json();
+    error.status = response.status;
+    throw error;
+  }
+  return response.json();
+}
 
 export default function App({ Component, pageProps }) {
+  const { data, isLoading, error } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+
+  console.log(data);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} data = {data} />
     </>
   );
 }

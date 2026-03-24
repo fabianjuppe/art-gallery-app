e274040801234315b82833b903c893f63735d46d
Fabian <65469588+fabianjuppe@users.noreply.github.com>
2026-03-24 16:18:47 +0100

    styled nav

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,19 +1,61 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+
+const StyledList = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  gap: 8rem;
+  padding: 1rem;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #8c8c8c;
+  color: ${({ $isActive }) => ($isActive ? "#000000" : "#8c8c8c")};
+  font-size: 2rem;
+
+  &:hover {
+    color: #000000;
+  }
+`;
+
+const StyledNav = styled.nav`
+  position: fixed;
+  bottom: 3rem;
+  width: 100%;
+  background-color: #ffffff;
+`;
 
 export default function Navigation() {
+  const router = useRouter();
+
   return (
-    <>
-      <ul>
+    <StyledNav>
+      <StyledList>
         <li>
-          <Link href="/gallery">Gallery</Link>
+          <StyledLink
+            href="/gallery"
+            $isActive={router.pathname === "/gallery"}
+          >
+            Overview
+          </StyledLink>
         </li>
         <li>
-          <Link href="/">Spotlight</Link>
+          <StyledLink href="/" $isActive={router.pathname === "/"}>
+            Böker & Juppe
+          </StyledLink>
         </li>
         <li>
-          <Link href="/favorites">Favoriten</Link>
+          <StyledLink
+            href="/favorites"
+            $isActive={router.pathname === "/favorites"}
+          >
+            Favorites
+          </StyledLink>
         </li>
-      </ul>
-    </>
+      </StyledList>
+    </StyledNav>
   );
 }
